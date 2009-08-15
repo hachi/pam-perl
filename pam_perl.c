a06566050795a9fdd38730e082ec818423edd61b
Jonathan Steinert <hachi@kuiki.net>
2009-08-14 22:01:48 -0400

    Break perl call into a seperate function, refactor.

--- pam_perl.c
+++ pam_perl.c
@@ -9,8 +9,10 @@
 #include <security/pam_modules.h>
 #include <security/_pam_macros.h>
 
-PAM_EXTERN int
-pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
+int invoke(const char *phase, pam_handle_t *pamh, int flags, int argc, const char **argv);
+
+int
+invoke(const char *phase, pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
     int my_argc = 3;
     char *my_argv[] = { "", "-T", "-e1", NULL }; // POSIX says it must be NULL terminated, even though we have argc
@@ -50,37 +52,48 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
         PL_curinterp = original_interpreter;
     }
 
+}
+
+PAM_EXTERN int
+pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    invoke("authenticate", pamh, flags, argc, argv);
     return PAM_SUCCESS;
 }
 
 PAM_EXTERN int
 pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-     return PAM_SUCCESS;
+    invoke("setcred", pamh, flags, argc, argv);
+    return PAM_SUCCESS;
 }
 
 PAM_EXTERN int
 pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-     return PAM_SUCCESS;
+    invoke("acct_mgmt", pamh, flags, argc, argv);
+    return PAM_SUCCESS;
 }
 
 PAM_EXTERN int
 pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-     return PAM_SUCCESS;
+    invoke("chauthtok", pamh, flags, argc, argv);
+    return PAM_SUCCESS;
 }
 
 PAM_EXTERN int
 pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
+    invoke("open_session", pamh, flags, argc, argv);
     return PAM_SUCCESS;
 }
 
 PAM_EXTERN int
 pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-     return PAM_SUCCESS;
+    invoke("close_session", pamh, flags, argc, argv);
+    return PAM_SUCCESS;
 }
 
 #ifdef PAM_STATIC
