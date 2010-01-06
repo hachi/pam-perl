f46eb3b2e309ebf3ccc12b4c57eaccd722db62c7
Jonathan Steinert <hachi@kuiki.net>
2010-01-05 20:34:15 -0800

    Switch to 5.10.1 and -Wall

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -x
 set -e
 
-gcc -I/usr/lib/perl/5.10.0/CORE/ -g -fPIC -c pam_perl.c
-gcc -I/usr/lib/perl/5.10.0/CORE/ -L/usr/lib/perl/5.10.0/CORE/ -shared -lpam -lperl -lm -o pam_perl.so pam_perl.o
+gcc -Wall -I/usr/lib/perl/5.10.1/CORE/ -g -fPIC -c pam_perl.c
+gcc -Wall -I/usr/lib/perl/5.10.1/CORE/ -L/usr/lib/perl/5.10.1/CORE/ -shared -lpam -lperl -lm -o pam_perl.so pam_perl.o
