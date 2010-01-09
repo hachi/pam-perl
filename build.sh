0eacd640a288160704b591c963754f3ada804397
Jonathan Steinert <hachi@kuiki.net>
2010-01-09 00:47:38 -0800

    Make it a little easier to swap perl version.

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 set -x
 set -e
 
-gcc $(perl -MExtUtils::Embed -e ccopts) -Wall -g -fPIC -c pam_perl.c
-gcc $(perl -MExtUtils::Embed -e ldopts) -shared -lpam -o pam_perl.so pam_perl.o
+PERL="perl"
+
+gcc $($PERL -MExtUtils::Embed -e ccopts) -Wall -g -fPIC -c pam_perl.c
+gcc $($PERL -MExtUtils::Embed -e ldopts) -shared -lpam -o pam_perl.so pam_perl.o
