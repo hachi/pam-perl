f1a3e3c4731bf413a6cdb7e93427135dbae34227
Jonathan Steinert <hachi@kuiki.net>
2012-03-28 23:59:14 -0700

    On darwin pam_set_data and pam_get_data are in pam_appl.h

--- compat.h
+++ compat.h
@@ -2,6 +2,7 @@
 #define _PAM_PERL_COMPAT
 
 #ifdef PERL_DARWIN
+#include <security/pam_appl.h>
 void pam_syslog(pam_handle_t *pamh, int level, const char *msg, ...) { }
 #else
 #include <security/pam_ext.h>
